964557939bd1b34af4e177ebc6ced44a7ee35a08
IsmailBadawy <ibadawy96@gmail.com>
2025-01-24 20:18:44 +0200

    new edited

---
+++ js/language.js
@@ -0,0 +1,143 @@
+const texts = {
+    en: {
+        title: "QAS",
+        desc: "Digital transformation of quality system",
+        home: "Home",
+        aboutUs: "About Us",
+        products: "Products",
+        contactUs: "Contact Us",
+        hint: "Integrated Intelligent Quality System (i2QS) software for handling quality management system and accreditation standards implementation.",
+        learnMore: "Learn More",
+        carousel1: "A dynamic framework enhancing performance and quality in application development.",
+        carousel2: "Continuous learning is the foundation of quality improvement and excellence.",
+        carousel3: "Personal goals aligned with quality reflect sustainable success.",
+        carousel4: "Integrated applications boosting operational efficiency and quality outcomes.",
+        carousel5: "Performance testing: Speed and accuracy for achieving top quality standards.",
+        carousel6: "Constant updates: A cornerstone for quality enhancement.",
+        carousel7: "A professional website reflecting high-quality standards.",
+        about_text: "QAS was established in 2014, based in Cairo, Egypt. We provide a complete consultation and software solutions to the healthcare organizations in order to enhance the provided services and to facilitate the process of quality management system implementation.",
+        about_text2: "Our solutions and services comply with the national and international quality and accreditation standard/s as well as the applicable guidelines.",
+        why_text: "Why Choose Us ?",
+        why_experience_text: "QAS team has a long experience in process enhancement, workflow, software development as well as quality and accreditation. We are committed to collaborate with our customers to be a partner in success and for seeking for excellence.",
+        experience: "Experience",
+        why_vision_text: "We aim to be a leader service provider that supports the integration of the healthcare processes and quality system implementation through digital transformation.",
+        vision: "Vision",
+        why_mission_text: "QAS is a service provided in the field of software and consultation services related to the healthcare sector.",
+        mission: "Mission",
+        explore: "Explore Our Products",
+        iqps_title: "IQPS",
+        iqps_text: "IQPS is an integrated, intelligent and customized software can be applied to implement the quality management system in the healthcare organizations.",
+        iqps_benefits: "IQPS ensures and maintains that: Total digitalization of QMS and accreditation requirements; Compliance with quality and accreditation standards; Integrity of the quality management system; Data security and confidentiality; Saving time during the implementation, data collection, and analysis; Easy handling of QMS Documentation and implementation; Providing accurate reports at all levels of management.",
+        contact_info_title: "Contact Info",
+        contact_phone: "Phone: +20 0100000000",
+        contact_address: "Address: Arkan Building 4, floor 4, Regus office# 307, Sheikh Zayed, Giza, Egypt.",
+        contact_follow_us: "Follow us on:",
+        contact_social_links: "Twitter, Facebook, Instagram, LinkedIn",
+        footer_links_title: "Contact Info",
+        footer_links: "About Us, Products, Contact Us",
+        footer_help_title: "Help Center",
+        footer_help_links_faq: "FQ&A",
+        footer_help_links_customer_service: "Customer Service",
+        footer_help_links_privacy_policy: "Privacy Policy",
+        footer_help_links_terms_of_use: "Terms of Use",
+        footer_help_links_cookies_settings: "Cookies Settings",
+        footer_stay_in_touch: "Let's Stay in Touch",
+        footer_email_placeholder: "Your Email Address",
+        footer_phone_placeholder: "Your Phone Number",
+        footer_comment_placeholder: "Write Your Comment Here",
+        footer_submit_button: "Submit",
+        footer_copyright: "QAS 2014 -2023",
+        footer_rights: "All rights Reserved @QAS",
+        devloped:"Devloped By Ismail Badawy"
+    },
+    ar: {
+        title: "نظام الجودة الشامل",
+        desc: "التحول الرقمي لنظام الجودة",
+        home: "القائمة الرئيسية",
+        aboutUs: "معلومات عنا",
+        products: "المنتجات",
+        contactUs: "اتصل بنا",
+        hint: "نظام الجودة الذكي المتكامل (i2QS) لإدارة نظام إدارة الجودة وتنفيذ معايير الاعتماد..",
+        learnMore: "تعرف علي المزيد",
+        carousel1: "إطار عمل ديناميكي يعزز الأداء والجودة في تطوير التطبيقات.",
+        carousel2: "التعلم المستمر هو أساس تحسين الجودة والتميز.",
+        carousel3: "الأهداف الشخصية المتماشية مع الجودة تعكس نجاحاً مستداماً.",
+        carousel4: "تطبيقات متكاملة تعزز الكفاءة التشغيلية ونتائج الجودة.",
+        carousel5: "اختبارات الأداء: السرعة والدقة لتحقيق معايير الجودة العالية.",
+        carousel6: "التحديثات المستمرة: حجر الأساس لتحسين الجودة.",
+        carousel7: "موقع احترافي يعكس معايير جودة عالية.",
+        about_text: "تأسست QAS في عام 2014، ومقرها في القاهرة، مصر. نقدم حلولاً استشارية وبرمجية شاملة للمؤسسات الصحية بهدف تحسين الخدمات المقدمة وتسهيل عملية تنفيذ نظام إدارة الجودة.",
+        about_text2: "تتوافق حلولنا وخدماتنا مع معايير الجودة والاعتماد الوطنية والدولية بالإضافة إلى الإرشادات المعمول بها.",
+        why_text: "  لماذا نحن ؟ ",
+        why_experience_text: "يمتلك فريق QAS خبرة طويلة في تحسين العمليات، وتطوير سير العمل، وتطوير البرمجيات، فضلاً عن الجودة والاعتماد. نحن ملتزمون بالتعاون مع عملائنا لنكون شركاء في النجاح والسعي لتحقيق التميز.",
+        experience: "خبراتنا",
+        why_vision_text: "نسعى لأن نكون مزود خدمة رائد يدعم تكامل العمليات الصحية وتنفيذ نظام الجودة من خلال التحول الرقمي.",
+        vision: "حلمنا",
+        why_mission_text: "QAS هي خدمة مقدمة في مجال البرمجيات والخدمات الاستشارية المتعلقة بقطاع الرعاية الصحية.",
+        mission: "مهمتنا",
+        explore: "اكتشف منتجاتنا",
+        iqps_title: "نظام IQPS",
+        iqps_text: "نظام IQPS هو برنامج ذكي ومتكامل ومخصص يمكن تطبيقه لتنفيذ نظام إدارة الجودة في المؤسسات الصحية.",
+        iqps_benefits: "نظام IQPS يضمن ويحافظ على: الرقمنة الكاملة لمتطلبات نظام إدارة الجودة والاعتماد؛ الالتزام بمعايير الجودة والاعتماد؛ تكامل نظام إدارة الجودة؛ أمان وسرية البيانات؛ توفير الوقت أثناء التنفيذ وجمع البيانات وتحليلها؛ سهولة التعامل مع توثيق نظام إدارة الجودة وتنفيذه؛ تقديم تقارير دقيقة على جميع مستويات الإدارة.",
+        contact_info_title: "معلومات التواصل",
+        contact_phone: "الهاتف: +20 0100000000",
+        contact_address: "العنوان: مبنى أركان 4، الطابق 4، مكتب ريجس #307، الشيخ زايد، الجيزة، مصر.",
+        contact_follow_us: "تابعنا على:",
+        contact_social_links: "تويتر، فيسبوك، إنستجرام، لينكد إن",
+        footer_links_title: "معلومات التواصل",
+        footer_links: "عن الشركة، المنتجات، تواصل معنا",
+        footer_help_title: "مركز المساعدة",
+        footer_help_links_faq: "الأسئلة الشائعة",
+        footer_help_links_customer_service: "خدمة العملاء",
+        footer_help_links_privacy_policy: "سياسة الخصوصية",
+        footer_help_links_terms_of_use: "شروط الاستخدام",
+        footer_help_links_cookies_settings: "إعدادات الكوكيز",
+        footer_stay_in_touch: "لنبقى على تواصل",
+        footer_email_placeholder: "بريدك الإلكتروني",
+        footer_phone_placeholder: "رقم هاتفك",
+        footer_comment_placeholder: "اكتب تعليقك هنا",
+        footer_submit_button: "إرسال",
+        footer_copyright: "QAS 2014 -2023",
+        footer_rights: "جميع الحقوق محفوظة @QAS",
+        devloped:"تم التصميم والتطوير بواسطة إسماعيل بدوي"
+    }
+};
+
+const switcher = document.getElementById("languageSwitcher");
+let currentLang = "en";
+
+// استرجاع اللغة المحفوظة عند تحميل الصفحة
+document.addEventListener("DOMContentLoaded", () => {
+    const savedLang = localStorage.getItem("selectedLanguage");
+    if (savedLang) {
+        currentLang = savedLang;
+        updateTexts(currentLang);
+        switcher.textContent = currentLang === "en" ? "عربي" : "English";
+
+        // ضبط الاتجاه والمحاذاة
+        document.body.dir = currentLang === "ar" ? "rtl" : "ltr";
+        document.body.style.textAlign = currentLang === "ar" ? "right" : "left";
+    }
+});
+
+switcher.addEventListener("click", () => {
+    currentLang = currentLang === "en" ? "ar" : "en";
+    updateTexts(currentLang);
+    switcher.textContent = currentLang === "en" ? "عربي" : "English";
+
+    // حفظ اللغة في localStorage
+    localStorage.setItem("selectedLanguage", currentLang);
+});
+
+function updateTexts(lang) {
+    document.querySelectorAll("[data-lang]").forEach(element => {
+        const key = element.getAttribute("data-lang");
+        if (texts[lang][key]) {
+            element.textContent = texts[lang][key];
+        }
+    });
+
+    // تغيير الاتجاه والمحاذاة
+    document.body.dir = lang === "ar" ? "rtl" : "ltr";
+    document.body.style.textAlign = lang === "ar" ? "right" : "left";
+}
---
+++ js/mood.js
@@ -0,0 +1,60 @@
+// Define the color modes
+const orangeMode = {
+    mainColorBody: '#FFF8F0',
+    fontColor: '#D97706',
+    seconderyColor: '#4b3723',
+    btnColor: '#F97316',
+    hoverColor: '#f59958',
+    borderColor: '#FFEDD5',
+    backgroundImage: 'url(../images/background-orange.svg)'
+
+};
+
+const greenMode = {
+    mainColorBody: '#F0FFF4',
+    fontColor: '#2D6A4F',
+    seconderyColor: '#34473d',
+    btnColor: '#52B788',
+    hoverColor: '#78d1a9c7',
+    borderColor: '#A3E635',
+    backgroundImage: 'url(../images/background-green.svg)'
+
+};
+
+const navyMode = {
+    mainColorBody: '#F8FAFC',
+    fontColor: '#1E293B',
+    seconderyColor: '#034eb8',
+    btnColor: '#334155',
+    hoverColor: '#4e6380',
+    borderColor: '#CBD5E1',
+    backgroundImage: 'url(../images/background-navy.svg)'
+
+};
+
+// Get elements
+const orangeBtn = document.getElementById('orange-mode');
+const greenBtn = document.getElementById('green-mode');
+const navyBtn = document.getElementById('navy-mode');
+
+function applyTheme(theme) {
+    Object.keys(theme).forEach(key => {
+        if (key === 'backgroundImage') {
+            document.body.style.backgroundImage = theme[key];
+        } else {
+            document.documentElement.style.setProperty(`--${key}`, theme[key]);
+        }
+    });
+    localStorage.setItem('selectedTheme', JSON.stringify(theme));
+}
+
+
+const savedTheme = localStorage.getItem('selectedTheme');
+if (savedTheme) {
+    applyTheme(JSON.parse(savedTheme));
+}
+
+// Event Listeners for buttons
+orangeBtn.addEventListener('click', () => applyTheme(orangeMode));
+greenBtn.addEventListener('click', () => applyTheme(greenMode));
+navyBtn.addEventListener('click', () => applyTheme(navyMode));
